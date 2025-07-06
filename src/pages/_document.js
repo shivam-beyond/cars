bc324effa609b5b1fdcfaa16d6ed1a1c204fc7b5
shivam-beyond <shivam.l@beyondseek.com>
2025-07-06 17:33:29 +0530

    fix: update class to className in logo SVG and enable dark mode support

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head>
         <title>Flexcar</title>
         <link rel="icon" href="/favicon.png" />
